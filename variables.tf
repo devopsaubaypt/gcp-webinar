fc508d8edb3022fe9b18f5e364032074b8277697
Eduardo Hass <eduardo.hass@outlook.com>
2022-09-15 18:52:53 +0100

    cicd: add default values for variables

--- variables.tf
+++ variables.tf
@@ -1,20 +1,29 @@
+variable "region" {
+  description = "GCP region name"
+  type        = string
+  default     = "europe-west1"
+}
+
 variable "zone" {
   description = "Zone name"
   type        = string
+  default     = "europe-west1-b"
 }
 
 variable "project_id" {
   description = "GCP project identifier"
   type        = string
-}
-
-variable "region" {
-  description = "GCP region name"
-  type        = string
+  default     = "comphass-lab-001"
 }
 
 variable "storage_class" {
   description = "Class of storage of bucket STANDARD/NEARLINE/COLDLINE/ARCHIVE"
   type        = string
   default     = "STANDARD"
+}
+
+variable "bucket_name" {
+  description = "bucket_name"
+  type        = string
+  default     = "aubay-webinar-terraform-state"
 }
